a75f9b84d2a0f2d82be5322decc01959e86349b6
hadeedshahid <hadeedshahid08@gmail.com>
2023-04-19 16:36:13 +0500

    fixing signup creds

--- Components/Signup/SignupModal.js
+++ Components/Signup/SignupModal.js
@@ -18,13 +18,13 @@ const SignupModal=(props)=>{
         // console.log("public vercel",process.env.VERCEL_URL)
 
 
-        console.log("normal vercel",props.url)
-        await fetch(`https://${process.env.CUSTOM_URL}/api/auth/signup`,options)
+        // console.log("normal vercel",props.url)
+        await fetch(`https://${props.url}/api/auth/signup`,options)
         .then(res => res.json())
         .then(data => {
             console.log("***data", data);
             if (data) {
-                router.push(`http://${process.env.CUSTOM_URL}/LandingPage`);
+                router.push(`http://${props.url}/LandingPage`);
             }
         });
 
