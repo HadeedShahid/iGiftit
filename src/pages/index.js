b3be10c9b79fdbf5e89b7e5bb7b2d1ecaeb5c4cf
hadeed shahid <hadeed.shahid08@gmail.com>
2023-04-26 14:31:09 +0500

    still making landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,13 +15,25 @@ const LandingPage=()=>{
         </div>
       </div>
       <div className={styles.Hero}>
-        <div className={styles.Left}>
-          <div className={styles.Title}><span>Thoughtful</span> Gifting made easy<span className={styles.dot}>.</span></div>
-          <div className={styles.desc}>We aim to not deliver gifts, but to deliver love in a box to the people in your life who deserve it the most !</div>
-          <button className={styles.GetStarted}>
-            <span>Get Started</span>
-            <img src='/static/images/icons/heart.svg'></img>
-          </button>
+        <div className={styles.cont}>
+          <div className={styles.Left}>
+            <div className={styles.Title}><span>Thoughtful</span> Gifting made easy<span className={styles.dot}>.</span></div>
+            <div className={styles.desc}>We aim to not deliver gifts, but to deliver love in a box to the people in your life who deserve it the most !</div>
+            <button className={styles.GetStarted}>
+              <span className={styles.text}>Get Started</span>
+              <img src='/static/images/icons/heart.svg'></img>
+            </button>
+          </div>
+          <div className={styles.buff}></div>
+          <div className={styles.Right}>
+            <img src='/static/images/icons/Hero_Image.png'></img>
+            <img className={styles.rightImg} src='/static/images/icons/fav_per.png'></img>
+            <img className={styles.leftImg} src='/static/images/icons/car_chosen.png'></img>
+            <img className={styles.you} src='/static/images/icons/you.svg'></img>
+            {/* <img className={styles.line} src='/static/images/icons/line.svg'></img> */}
+
+          </div>
+          <div className={styles.buff1}></div>
         </div>
         
       </div>
